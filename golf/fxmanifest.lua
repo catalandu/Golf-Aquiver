9be831b36dc6730bc5c772403f853986e2a558c4
catalandu <84291747+catalandu@users.noreply.github.com>
2022-05-19 02:03:46 +0200

    Add files via upload

---
+++ golf/fxmanifest.lua
@@ -0,0 +1,38 @@
+version '1.0.0'
+author 'freamee'
+decription 'Aquiver golf'
+
+-- Leaked By: Leaking Hub | J. Snow | leakinghub.com
+
+client_scripts {
+    '@es_extended/locale.lua',
+    'locales/*.lua',
+    'config.lua',
+    'client/client.lua'
+}
+
+server_scripts {
+    '@es_extended/locale.lua',
+    'locales/*.lua',
+    'config.lua',
+    '@mysql-async/lib/MySQL.lua',
+    'server/server.lua'
+}
+
+dependencies {
+    'es_extended',
+    'mysql-async'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/js/*.js',
+    'html/DEP/*.js',
+    'html/img/**',
+    'html/ProximaNova.woff'
+}
+
+game 'gta5'
+fx_version 'adamant'
